afe4bcd86bd2816ab7ce6be68b517a3b547973e8
NguyenBang <bangtbtb@gmail.com>
2022-03-17 11:54:22 +0700

    Add feature: push file to another server by scp

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM postgresql:13
+FROM postgres:13
 
 WORKDIR /backup 
 COPY . .
@@ -6,8 +6,12 @@ COPY . .
 ENV TZ=Asia/Ho_Chi_Minh
 RUN mkdir /data
 
+RUN apt-get update && \
+    apt-get install -y ssh
+
 USER root
 
+ENV BACK_UP_LOCATION=/data
 
 
 CMD  ["/backup/entry-point.sh" ]
--- entry-point.sh
+++ entry-point.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
 
+
+BACK_UP_HOST=${BACK_UP_HOST:-10.60.0.53}
+BACK_UP_PORT=${BACK_UP_PORT:-5432}
+BACK_UP_USER=${BACK_UP_USER:-admin}
+BACK_UP_PASSWORD=${BACK_UP_PASSWORD:-244466666}
+BACK_UP_LOCATION=${BACK_UP_LOCATION:-.}
+BACK_UP_DATABASE_LIST=${BACK_UP_DATABASE_LIST:-banners,chart,prod_db,storage}
+
+# Use for scp
+SSH_KEY_FILE=${SSH_KEY_FILE:-""}
+SSH_HOST=${SSH_HOST:-""}
+SSH_LOCATION=${SSH_LOCATION:-""}
+
+export PGPASSWORD=$BACK_UP_PASSWORD
+
 # dump . 192.168.4.79 5432 admin 244466666 prod_db
 function dump() {
     local BACK_UP_LOCATION=$1
@@ -9,57 +24,37 @@ function dump() {
     local BACK_UP_PASSWORD=$5
     local BACK_UP_DATABASE=$6
 
-    NAME="$( date +"%Y.%m.%d-%T" )"
-    LOC=$BACK_UP_LOCATION/dump/$( date +"%Y.%m.%d" )
-    NAME="$BACK_UP_DATABASE-$NAME"
-    echo "$( date +"%Y.%m.%d-%T" ) [$LOC]/$NAME"
+    local TODAY=$( date +"%Y" )/$( date +"%m" )/$( date +"%d" )
+    local LOC=$BACK_UP_LOCATION/$TODAY
+    local NAME="$BACK_UP_DATABASE-$( date +"%H.%M" ).gz"
+
+    echo "$( date +"%Y.%m.%d-%H.%M" ) $LOC/$NAME"
     mkdir -p $LOC
-    PGPASSWORD=$BACK_UP_PASSWORD \
-        pg_dump -h $BACK_UP_HOST \
+    pg_dump -h $BACK_UP_HOST \
                 -U $BACK_UP_USER \
-                -d $BACK_UP_DATABASE | gzip  > $LOC/$NAME.gz
+                -d $BACK_UP_DATABASE | gzip  > $LOC/$NAME
+
+    if [[ ! -z "$SSH_KEY_FILE" ]]; then
+        ssh -o StrictHostKeyChecking=no -i $SSH_KEY_FILE $SSH_HOST "mkdir -p $SSH_LOCATION/$TODAY"
+        scp -o StrictHostKeyChecking=no -i $SSH_KEY_FILE $LOC/$NAME $SSH_HOST:$SSH_LOCATION/$TODAY
+    fi
 }
 
 
-# dump . 192.168.4.79 5432 admin 244466666 prod_db
-function dump_all() {
-    local BACK_UP_LOCATION=$1
-    local BACK_UP_HOST=$2
-    local BACK_UP_PORT=$3
-    local BACK_UP_USER=$4
-    local BACK_UP_PASSWORD=$5
-
-    local NAME="$( date +"%Y.%m.%d-%T" )"
-    local LOC=$BACK_UP_LOCATION/dump/$( date +"%Y.%m.%d" )
-
-    echo "$( date +"%Y.%m.%d-%T" ) [$LOC]/$NAME"
-    mkdir -p $LOC
-    PGPASSWORD=$BACK_UP_PASSWORD \
-        pg_dumpall -h $BACK_UP_HOST -U $BACK_UP_USER | gzip  > $LOC/$NAME.gz
-}
-
-
-BACK_UP_HOST=${BACK_UP_HOST:-192.168.4.79}
-BACK_UP_PORT=${BACK_UP_PORT:-5432}
-BACK_UP_USER=${BACK_UP_USER:-admin}
-BACK_UP_PASSWORD=${BACK_UP_PASSWORD:-244466666}
-BACK_UP_LOCATION=${BACK_UP_LOCATION:-.}
-BACK_UP_DATABASE_LIST=${BACK_UP_DATABASE_LIST:-banners,chart,prod_db}
-
-
-echo "URL:  postgres://$BACK_UP_USER:$BACK_UP_PASSWORD@$BACK_UP_HOST:$BACK_UP_PORT/$BACK_UP_DATABASE_LIST"
+# echo "URL:  postgres://$BACK_UP_USER:$BACK_UP_PASSWORD@$BACK_UP_HOST:$BACK_UP_PORT/$BACK_UP_DATABASE_LIST"
 while [ true ]; do
+    echo "$( date +"%Y.%m.%d-%H.%M" ) Start backup"
     IFS=',' read -r -a array <<< $BACK_UP_DATABASE_LIST
     for it_database in "${array[@]}"
     do
-        echo "$( date +"%Y.%m.%d-%T" ) Backup database $it_database"
+        echo "$( date +"%Y.%m.%d-%H.%M" ) Backup database $it_database"
         dump $BACK_UP_LOCATION $BACK_UP_HOST $BACK_UP_PORT \
             $BACK_UP_USER $BACK_UP_PASSWORD $it_database
-        echo "$( date +"%Y.%m.%d-%T" ) Backup database $it_database done"
+        echo "$( date +"%Y.%m.%d-%H.%M" ) Backup database $it_database done"
     done
 
 
-    echo "$( date +"%Y.%m.%d-%T" ) Backup database done. Wait for next time"
+    echo "$( date +"%Y.%m.%d-%H.%M" ) Backup database done. Wait for next time"
     while [ true ]; do 
         if [ "$( date +"%H" )" ==  "2" ]; then
             break
